9205e67c49970d9629260900dbd0ec44eeb55b06
deltaxwizard <samsu@129.21.43.82>
2021-03-09 12:58:52 -0500

    Initial working commit.

---
+++ src/main/java/net/mcreator/onedeathforall/procedures/CustomdeathProcedure.java
@@ -0,0 +1,102 @@
+package net.mcreator.onedeathforall.procedures;
+
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.DamageSource;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.onedeathforall.OnedeathforallModElements;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+
+@OnedeathforallModElements.ModElement.Tag
+public class CustomdeathProcedure extends OnedeathforallModElements.ModElement {
+	public CustomdeathProcedure(OnedeathforallModElements instance) {
+		super(instance, 1);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure Customdeath!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure Customdeath!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure Customdeath!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure Customdeath!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure Customdeath!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((entity instanceof PlayerEntity)) {
+			{
+				MinecraftServer mcserv = ServerLifecycleHooks.getCurrentServer();
+				if (mcserv != null)
+					mcserv.getPlayerList()
+							.sendMessage(new StringTextComponent((((entity.getDisplayName().getString())) + "" + ("ruined it for everyone..."))));
+			}
+			if (world instanceof ServerWorld)
+				((ServerWorld) world).addLightningBolt(new LightningBoltEntity(world.getWorld(), (int) x, (int) y, (int) z, true));
+			{
+				List<? extends PlayerEntity> _players = world.getPlayers();
+				for (Entity entityiterator : _players) {
+					if ((entityiterator.isAlive())) {
+						entityiterator.attackEntityFrom(DamageSource.GENERIC, (float) 20000);
+					}
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityDeath(LivingDeathEvent event) {
+		if (event != null && event.getEntity() != null) {
+			Entity entity = event.getEntity();
+			Entity sourceentity = event.getSource().getTrueSource();
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			World world = entity.world;
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("sourceentity", sourceentity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
